b4e928741168d981a0d38ed6bb13f63064eca9c1
Matias Viola <matiasvioladi@gmail.com>
2024-03-21 21:00:56 -0300

    feat: primera implementacion de la funcion 3.a en proximo_feriado.py

--- proximo_feriado.py
+++ proximo_feriado.py
@@ -38,14 +38,15 @@ class NextHoliday:
             holidays[0]
         )
 
-        self.loading = False
         self.holiday = holiday
 
     def fetch_holidays(self):
         """ Realiza la solicitud HTTP a la API para obtener los feriados del año actual."""
+        self.loading = True
         response = requests.get(get_url(self.year))
         data = response.json()
         self.set_next(data)
+        self.loading = False
 
     def render(self):
         """ Imprime la información sobre el próximo feriado. """
@@ -62,7 +63,41 @@ class NextHoliday:
             print("Tipo:")
             print(self.holiday['tipo'])
 
+    # ----------------------------------------------------------------
+    def proximo_feriado_del_tipo(self, type):
+        """ Devuelve el próximo feriado 'tipo'."""
+        return self.holiday[type]
+
+    def fetch_holidays_del_tipo(self, type):
+        """ Realiza la solicitud HTTP a la API para obtener los feriados 'tipo' del año actual."""
+        self.loading = True
+        response = requests.get(get_url(self.year))
+        data = response.json()
+        self.set_next_tipo(data, type)
+        self.loading = False
+
+    def set_next_tipo(self, holidays, type):
+        """ Establece el próximo feriado 'tipo'."""
+        now = date.today()
+        today = {
+            'day': now.day,
+            'month': now.month
+        }
+
+        holiday = next(
+            (h for h in holidays if h['tipo'] == type and h['mes'] == today['month']
+             and h['dia'] > today['day'] or h['mes'] > today['month']),
+            holidays[0]
+        )
+
+        self.loading = False
+        self.holiday = holiday
+
 
 next_holiday = NextHoliday()
 next_holiday.fetch_holidays()
+next_holiday.render()
+
+# next_holiday_del_tipo = NextHoliday()
+next_holiday.fetch_holidays_del_tipo('puente')
 next_holiday.render()
