6187f03083d46587c62f4233b0fbbffd44f2871e
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-03-22 20:56:41 -0300

    test: Se implementarons los pytests utilizando nuestra API y llegando a los casos borde.

--- test_pytest.py
+++ test_pytest.py
@@ -2,6 +2,9 @@ import requests
 import pytest
 import requests_mock
 
+BASE_URL_API = 'http://localhost:5000/peliculas'
+
+
 @pytest.fixture
 def mock_response():
     with requests_mock.Mocker() as m:
@@ -12,41 +15,180 @@ def mock_response():
         ])
 
         # Simulamos la respuesta para agregar una nueva película
-        m.post('http://localhost:5000/peliculas', status_code=201, json={'id': 3, 'titulo': 'Pelicula de prueba', 'genero': 'Acción'})
+        m.post('http://localhost:5000/peliculas', status_code=201,
+               json={'id': 3, 'titulo': 'Pelicula de prueba', 'genero': 'Acción'})
 
         # Simulamos la respuesta para obtener detalles de una película específica
-        m.get('http://localhost:5000/peliculas/1', json={'id': 1, 'titulo': 'Indiana Jones', 'genero': 'Acción'})
+        m.get('http://localhost:5000/peliculas/1',
+              json={'id': 1, 'titulo': 'Indiana Jones', 'genero': 'Acción'})
 
         # Simulamos la respuesta para actualizar los detalles de una película
-        m.put('http://localhost:5000/peliculas/1', status_code=200, json={'id': 1, 'titulo': 'Nuevo título', 'genero': 'Comedia'})
+        m.put('http://localhost:5000/peliculas/1', status_code=200,
+              json={'id': 1, 'titulo': 'Nuevo título', 'genero': 'Comedia'})
 
         # Simulamos la respuesta para eliminar una película
         m.delete('http://localhost:5000/peliculas/1', status_code=200)
 
         yield m
 
-def test_obtener_peliculas(mock_response):
-    response = requests.get('http://localhost:5000/peliculas')
+# Las pruebas con el sufijo "WHIH_MOCK" son las que utilizan el mock.
+
+
+def test_obtener_peliculas_WHIH_MOCK(mock_response):
+    response = requests.get(BASE_URL_API)
     assert response.status_code == 200
     assert len(response.json()) == 2
 
-def test_agregar_pelicula(mock_response):
+
+def test_agregar_pelicula_WHIH_MOCK(mock_response):
     nueva_pelicula = {'titulo': 'Pelicula de prueba', 'genero': 'Acción'}
-    response = requests.post('http://localhost:5000/peliculas', json=nueva_pelicula)
+    response = requests.post(BASE_URL_API, json=nueva_pelicula)
     assert response.status_code == 201
     assert response.json()['id'] == 3
 
-def test_obtener_detalle_pelicula(mock_response):
-    response = requests.get('http://localhost:5000/peliculas/1')
+
+def test_obtener_detalle_pelicula_WHIH_MOCK(mock_response):
+    response = requests.get(BASE_URL_API + '/1')
     assert response.status_code == 200
     assert response.json()['titulo'] == 'Indiana Jones'
 
-def test_actualizar_detalle_pelicula(mock_response):
+
+def test_actualizar_detalle_pelicula_WHIH_MOCK(mock_response):
     datos_actualizados = {'titulo': 'Nuevo título', 'genero': 'Comedia'}
-    response = requests.put('http://localhost:5000/peliculas/1', json=datos_actualizados)
+    response = requests.put(BASE_URL_API + '/1', json=datos_actualizados)
     assert response.status_code == 200
     assert response.json()['titulo'] == 'Nuevo título'
 
-def test_eliminar_pelicula(mock_response):
-    response = requests.delete('http://localhost:5000/peliculas/1')
+
+def test_eliminar_pelicula_WHIH_MOCK(mock_response):
+    response = requests.delete(BASE_URL_API + '/1')
     assert response.status_code == 200
+
+
+# PRUEBAS SIN MOCK, asegurarse de tener el servidor corriendo (python3 main.py).
+
+def test_obtener_peliculas():
+    response = requests.get(BASE_URL_API)
+    assert response.status_code == 200
+
+
+def test_agregar_y_eliminar_pelicula():
+    nueva_pelicula = {'titulo': 'Pelicula de prueba', 'genero': 'Acción'}
+    response = requests.post(BASE_URL_API, json=nueva_pelicula)
+    assert response.status_code == 201
+    assert response.json()['titulo'] == nueva_pelicula['titulo']
+    assert response.json()['genero'] == nueva_pelicula['genero']
+
+
+def test_eliminar_pelicula():
+    response = requests.delete(BASE_URL_API + '/10')
+    assert response.status_code == 200
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con id=50000.
+    response = requests.delete(BASE_URL_API + '/50000')
+    assert response.status_code == 404
+    response = requests.delete(BASE_URL_API + '/-1')
+    assert response.status_code == 404
+
+
+def test_obtener_detalle_pelicula():
+    response = requests.get(BASE_URL_API + '/1')
+    assert response.status_code == 200
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con id=50000.
+    response = requests.get(BASE_URL_API + '/50000')
+    assert response.status_code == 404
+    response = requests.get(BASE_URL_API + '/-1')
+    assert response.status_code == 404
+
+
+def test_actualizar_detalle_pelicula():
+    datos_actualizados = {'titulo': 'Nuevo título', 'genero': 'Comedia'}
+    response = requests.put(BASE_URL_API + '/1', json=datos_actualizados)
+    assert response.status_code == 200
+    assert response.json()['titulo'] == datos_actualizados['titulo']
+    assert response.json()['genero'] == datos_actualizados['genero']
+    assert response.json()['id'] == 1
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con id=50000.
+    datos_actualizados = {'titulo': 'Nuevo título', 'genero': 'Comedia'}
+    response = requests.put(BASE_URL_API + '/50000', json=datos_actualizados)
+    assert response.status_code == 404
+    response = requests.put(BASE_URL_API + '/-1', json=datos_actualizados)
+    assert response.status_code == 404
+
+
+def test_obtener_peliculas_por_genero():
+    genero = "Aventura"
+    response = requests.get(BASE_URL_API + '/genero/' + genero)
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con genero="genero_inexistente".
+    genero = "genero_inexistente"
+    response = requests.get(BASE_URL_API + '/genero/' + genero)
+    assert response.status_code == 404
+
+
+def test_obtener_peliculas_entitulo():
+    texto = "The"
+    response = requests.get(BASE_URL_API + '/entitulo/' + texto)
+    assert response.status_code == 200
+    assert len(response.json()) == 4
+
+    texto_con_espacios = "The Dark"
+    response = requests.get(BASE_URL_API + '/entitulo/' + texto_con_espacios)
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con texto="texto_inexistente".
+    texto = "texto_inexistente"
+    response = requests.get(BASE_URL_API + '/entitulo/' + texto)
+    assert response.status_code == 404
+
+
+def test_obtener_pelicula_random():
+    response = requests.get(BASE_URL_API + '/random')
+    assert response.status_code == 200
+    assert response.json() is not None
+
+
+def test_obtener_pelicula_random_por_genero():
+    genero = "Acción"
+    response = requests.get(BASE_URL_API + '/random/' + genero)
+    assert response.status_code == 200
+    assert response.json() is not None
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con genero="genero_inexistente".
+    genero = "genero_inexistente"
+    response = requests.get(BASE_URL_API + '/random/' + genero)
+    assert response.status_code == 404
+
+
+def test_obtener_pelicula_para_feriado_del_genero():
+    genero = "Acción"
+    response = requests.get(BASE_URL_API + '/proximoferiado/' + genero)
+    assert response.status_code == 200
+    assert response.json() is not None
+    assert response.json()['pelicula'] is not None
+    assert response.json()['proximo_feriado'] is not None
+
+    # TESTS QUE VALIDAN INFORMACION NO VALIDA.
+    # Ejecutar este test en el entorno local que
+    # sabemos que no existe una pelicula con genero="genero_inexistente".
+    genero = "genero_inexistente"
+    response = requests.get(BASE_URL_API + '/proximoferiado/' + genero)
+    assert response.status_code == 404
+    
